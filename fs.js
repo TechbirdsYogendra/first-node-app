5f516c42922f042f40a5fccefc4ed9cd62fa9b06
Yogendra Singh <000yogi1990@gmail.com>
2025-01-12 12:27:46 +0530

    1. Created new directory 2. Inside directory created file and write text and append text.

--- fs.js
+++ fs.js
@@ -17,9 +17,34 @@ fs.appendFileSync(fileName, 'Hare Krishna Hare Krishna Krishna Krishna Hare Hare
 // Deleting a file
 // fs.unlinkSync(fileName);
 
+const dirName = 'exampleDir';
+const newFileNameInsideDirectory = `${dirName}/newfile.txt`;
+
+// Creating a directory
+try {
+    fs.mkdirSync(dirName); 
+    console.log('Directory created successfully');
+} catch (error) {
+    if (error.code !== 'EEXIST') {
+        console.log(`Error while creating directory: ${error}`);
+    } else {
+        console.log('Directory already exists');
+    }
+}
+
+// Creating a file inside the directory
+try {
+    fs.writeFileSync(newFileNameInsideDirectory, 'Jai Radha Madhav Jai Kunj Bihari\n');
+    // Appending to a file inside the directory
+    fs.appendFileSync(newFileNameInsideDirectory, 'Jai Gopijan Vallabh Jai Girivar Dhari\n');
+    fs.appendFileSync(newFileNameInsideDirectory, 'Yashoda Nandan Braj Jan Ranjan\nYamuna Teer Van Chari\nHare Krishna Hare Krishna Krishna Krishna Hare Hare\nHare Rama Hare Rama Rama Rama Hare Hare\n');
+} catch (error) {
+    console.log(`Error while creating file: ${error}`);
+}
+
 // Reading from a file with error handling
 try {
-    const fileContent = fs.readFileSync(fileName, 'utf8');
+    const fileContent = fs.readFileSync(newFileNameInsideDirectory, 'utf8');
     console.log(fileContent); 
 } catch (error) {
    console.log(`Error while reading file ${error}`); 
